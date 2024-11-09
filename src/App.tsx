edf5ec86e9b81c97f123c99156f078737dbbb9e7
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-11-09 09:04:40 +0200

    Add basic app interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 // import env key
 
+import { useState } from "react";
+
 const OPEN_AI_KEY = import.meta.env.VITE_OPEN_AI_KEY;
 
-console.log("OPEN_AI_KEY:", OPEN_AI_KEY);
-
 const App = () => {
-  return <></>;
+  // TODO: implement chat history
+  const [chatHistory, setChatHistory] = useState<string[]>([]);
+
+  // TODO: implement send function
+  const send = async () => {
+    console.log("Send");
+  };
+
+  return (
+    <>
+      {/* TODO: implement UI interface for the text history */}
+
+      <input type="text" placeholder="Message ChatGPT" />
+      <button onClick={send}>Send</button>
+    </>
+  );
 };
 
 export default App;
